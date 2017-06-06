3a7a2dff1788e5fd948ecc21805d019d49da2433
Ramiro6 <fresco_esp@hotmail.com>
2017-06-06 11:24:47 +0200

    routes

--- angular-rails/freelance-camp-fe/src/app/app.module.ts
+++ angular-rails/freelance-camp-fe/src/app/app.module.ts
@@ -4,14 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',  component: HomeComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(routes),
     HttpModule
   ],
   providers: [],
