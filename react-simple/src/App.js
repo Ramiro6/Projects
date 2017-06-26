65860aa1cd311247eefc85716678c015a6822b8d
Ramiro6 <fresco_esp@hotmail.com>
2017-06-26 17:23:07 +0200

    new react

--- react-simple/src/App.js
+++ react-simple/src/App.js
@@ -3,6 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.nombre = "Ramiro Ruiz"
+  }
+
+
+
   render() {
     return (
       <div className="App">
@@ -10,9 +17,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <p>{this.nombre}</p>
       </div>
     );
   }
---
+++ react-simple/src/components/FilterableProductTable.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class FilterableProductTable extends Component {
+  constructor(){
+    super()
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
+}
+
+export default 
