88578fe44e0d4191b205650c3210436dd3d02a1a
Ramiro6 <fresco_esp@hotmail.com>
2017-06-30 13:06:51 +0200

    fole_angular

--- helloreact/src/App.js
+++ helloreact/src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Router, Route, Link, hashHistory } from 'react-router';
+
+const Home = () => <div><h1>Home</h1></div>;
+const About = () => <div><h1>About</h1></div>;
+const Contact = () => <div><h1>Contact</h1></div>;
 
 class App extends Component {
+  constructor(){
+    super()
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,11 +20,16 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          <h1>Ramiro Ruiz</h1>
         </p>
+        <Router history={ hashHistory }>
+          <Route path="/" component={Home}></Route>
+          <Route path="/about" component={About}></Route>
+          <Route path="/contact" component={Contact}></Route>
+        </Router>
       </div>
     );
   }
 }
+
 
 export default App;
