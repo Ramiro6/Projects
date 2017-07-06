aaf4325033c9af3d8c8e69c160161b83df7aba44
Ramiro6 <fresco_esp@hotmail.com>
2017-07-06 16:22:12 +0200

    router

--- Angular/6. miscelaneos/src/app/app.module.ts
+++ Angular/6. miscelaneos/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NgStyleComponent } from './components/ng-style/ng-style.component';
+import { ClasesComponent } from './components/clases/clases.component';
+import { ResaltadoDirective } from './directives/resaltado.directive';
+import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgStyleComponent,
+    ClasesComponent,
+    ResaltadoDirective,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/6. miscelaneos/src/app/directives/resaltado.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appResaltado]'
+})
+export class ResaltadoDirective {
+
+  constructor(private el:ElementRef) {
+    console.log("On")
+  }
+
+  @HostListener('mouseenter') mouseEntro(){
+    this.el.nativeElement.style.backgroundColor = "yellow"
+  }
+
+  @HostListener('mouseleave') mouseSalio(){
+    this.el.nativeElement.style.backgroundColor = null;
+  }
+
+}
